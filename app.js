93b79d747f09f7a0a1ef6a05231d53b9900db86a
tabrez510 <alamtabrez510@gmail.com>
2024-01-13 22:12:36 +0530

    routing request added

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-    // console.log(req.url, req.method, req.headers);
-    console.log('Tabrez');
+    res.setHeader('Content-Type', 'text/html')
+    const url = req.url;
+    if(url==='/'){
+        res.write('<html>')
+        res.write('<head><title>Node js</title></head>')
+        res.write('<body><h1>Welcome to my Node js Project</h1></body>')
+        res.write('</html>')
+        res.end();
+    }
+    else if(url==='/home'){
+        res.write('<html>')
+        res.write('<head><title>home</title></head>')
+        res.write('<body><h1>Welcome Home</h1></body>')
+        res.write('</html>')
+        res.end();
+    }
+    else if(url==='/about'){
+        res.write('<html>')
+        res.write('<head><title>about</title></head>')
+        res.write('<body><h1>Welcome to About Us Page</h1></body>')
+        res.write('</html>')
+        res.end();
+    }
+    else if(url==='/node'){
+        res.write('<html>')
+        res.write('<head><title>Node js</title></head>')
+        res.write('<body><h1>Welcome to my Node js Project</h1></body>')
+        res.write('</html>')
+        res.end();
+    }
 });
 server.listen(4000);
